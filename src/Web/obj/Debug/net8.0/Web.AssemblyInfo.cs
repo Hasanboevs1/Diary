e98455fedeefb34f565c09b955d9077e7b5da029
beam <friday555111@gmail.com>
2024-10-12 08:10:14 +0500

    some  changes

--- src/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ src/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcea1fc5522f68946670c96c785403b4cab8e041")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
